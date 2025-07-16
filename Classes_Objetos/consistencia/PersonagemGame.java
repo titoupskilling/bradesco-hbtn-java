170b6e6ae4584e4c519fa8ff0a6ec189f73ee56a
renato <ren.tito@yahoo.com.br>
2025-07-15 21:00:31 -0300

    project 2

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,54 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.nome = nome;
+        setSaudeAtual(saudeAtual);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (saudeAtual > 0) {
+            this.status = "vivo";
+        } else if (saudeAtual == 0) {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(nome != null && !nome.isEmpty()){
+            this.nome = nome;
+        }
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if ((saudeAtual - quantidadeDeDano) > 0) {
+            setSaudeAtual(saudeAtual - quantidadeDeDano);
+        } else {
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if ((saudeAtual + quantidadeDeCura) < 100) {
+            setSaudeAtual(saudeAtual + quantidadeDeCura);
+        } else {
+            setSaudeAtual(100);
+        }
+    }
+}
