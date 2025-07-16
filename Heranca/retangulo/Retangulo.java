edba07e5b31be01ce1075ac1d801466980c6f4b1
renato <ren.tito@yahoo.com.br>
2025-07-16 20:59:01 -0300

    project heranca

---
+++ Heranca/retangulo/Retangulo.java
@@ -0,0 +1,27 @@
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if(largura < 0.0){
+            throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    private double altura;
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if(largura < 0.0){
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+}
