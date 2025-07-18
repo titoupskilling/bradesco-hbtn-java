90659863f14981614fc789d509196129e24cb9b3
Renato <ren.tito@yahoo.com.br>
2025-07-18 02:24:32 +0000

    commit project heranca

--- Heranca_Exercicios/pedido_livraria/ItemPedido.java
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -4,7 +4,7 @@ public class ItemPedido {
     Produto produto;
     int quantidade;
 
-    ItemPedido(Produto produto, int quantidade) {
+    public ItemPedido(Produto produto, int quantidade) {
         this.produto = produto;
         this.quantidade = quantidade;
     }
--- Heranca_Exercicios/pedido_livraria/Pedido.java
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -1,10 +1,10 @@
-
+import produtos.Livro;
 
 public class Pedido {
     double percentualDesconto;
     ItemPedido[] itens;
 
-    Pedido(double percentualDesconto, ItemPedido[] itens) {
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
         this.percentualDesconto = percentualDesconto;
         this.itens = itens;
     }
@@ -15,5 +15,25 @@ public class Pedido {
             soma += itemPedido.quantidade * itemPedido.getProduto().obterPrecoLiquido();
         }
         return soma - (soma * (percentualDesconto / 100));
+    }
+
+    public void apresentarResumoPedido(){
+        System.out.printf("------- RESUMO PEDIDO -------\n");
+        for (ItemPedido itemPedido : itens) {
+            if(itemPedido.getProduto() instanceof Livro){
+                System.out.printf("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f",
+                itemPedido.getProduto().getClass(), 
+                itemPedido.getProduto().getTitulo(), 
+                itemPedido.getProduto().getPrecoBruto(),
+                itemPedido.getQuantidade(),
+                itemPedido.getQuantidade());
+            }
+        }
+        System.out.printf("----------------------------\n");
+        System.out.printf("DESCONTO: 6,67\n");
+        System.out.printf("TOTAL PRODUTOS: 133,36\n");
+        System.out.printf("----------------------------\n");
+        System.out.printf("TOTAL PEDIDO: 126,69\n");
+        System.out.printf("----------------------------\n");
     }
 }
