33104a650067b76be12b2f26e56e57d32b1cadb7
Renato <ren.tito@yahoo.com.br>
2025-07-24 01:02:42 +0000

    project interfaces

---
+++ Interfaces_Exercicios/workflow/atividades/CarregarVideo.java
@@ -0,0 +1,10 @@
+package atividades;
+
+public class CarregarVideo implements Atividade {
+
+    @Override
+    public void executar() {
+        System.out.println("carregando video");
+    }
+    
+}
