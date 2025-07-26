3216b81d10b2fec21ac24a098b3e45221925fd18
Renato <ren.tito@yahoo.com.br>
2025-07-26 13:12:15 +0000

    project exceptions

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,9 +1,9 @@
 public class Quadrado {
 
-    public static double area(double lado){
+    public static double area(double lado) throws java.lang.IllegalArgumentException {
         double resultado = 0.0;
         if(lado < 0){
-            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo.");
+            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo");
         } else {
             resultado = lado * lado;
         }
