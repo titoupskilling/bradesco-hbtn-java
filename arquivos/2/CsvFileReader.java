12052e3021e427651f9857bfdafe9ccab7a38d14
Renato <ren.tito@yahoo.com.br>
2025-07-31 21:43:38 +0000

    project arquivos

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+        String fileName = "funcionarios.csv"; // Nome do arquivo fixo para leitura
+        try (BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
+            String linhaStr = bf.readLine();
+            while (linhaStr != null) {
+                String[] linhaArray = linhaStr.split(",");
+                System.out.println(String.format("Funcionário: %s", linhaArray[0]));
+                System.out.println(String.format("Idade: %s", linhaArray[1]));
+                System.out.println(String.format("Departamento: %s", linhaArray[2]));
+                System.out.println(String.format("Salarial: %s", linhaArray[3]));
+                System.out.println("------------------------");
+                linhaStr = bf.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("Leitura do arquivo concluída.");
+    }
+}
