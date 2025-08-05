da2cd6fac8cfa880428a45d00b9d53d97e88dc71
Renato <ren.tito@yahoo.com.br>
2025-08-05 02:01:39 +0000

    project estrutura de dados

---
+++ complexidade/0/PessoasArray.java
@@ -0,0 +1,29 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de pesquisaConstante
+    public void pesquisaTempoConstante(int id){
+        if(id < this.nomes.length) {
+            String retorno = this.nomes[id];
+            System.out.println(String.format("Nome pesquisado é %s que está na posição %d", retorno, id));
+        } else {
+            throw new IllegalArgumentException(String.format("O array de nomes possui :%d nomes.", this.nomes.length));
+        }
+    }
+}
