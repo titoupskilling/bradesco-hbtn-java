a92a65d5ed9424cdbd4560596ac9c6093118f4a0
Renato <ren.tito@yahoo.com.br>
2025-08-12 00:36:42 +0000

    project estrutura de dados

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Pedido {
+
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas() {
+        int retorno = 0;
+        for (int i = 0; i < cookies.size(); i++) {
+            retorno =+ cookies.get(i).getQuantidadeCaixas();
+        }
+        return retorno;
+    }
+
+    public int removerSabor(String sabor) {
+        int retorno = 0;
+        Iterator<PedidoCookie> iterator = cookies.iterator();
+        while (iterator.hasNext()) {
+            PedidoCookie item = iterator.next();
+            if(item.getSabor().equals(sabor)){
+                retorno = item.getQuantidadeCaixas();
+                iterator.remove();
+            }
+        }
+        return retorno;
+    }
+
+    public List<PedidoCookie> getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(List<PedidoCookie> cookies) {
+        this.cookies = cookies;
+    }
+
+}
