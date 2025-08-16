13969f0508db72109a9bbc24b01a1dab28490b7f
Renato <ren.tito@yahoo.com.br>
2025-08-16 00:55:42 +0000

    project estrutura de dados

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,7 @@
+public class FolhaPagamento {
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculador) {
+        return calculador.calcular(funcionario.getSalario());
+    }
+    
+}
