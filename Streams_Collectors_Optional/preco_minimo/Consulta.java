ce9a0916b6645dbbcc49f35e41af4e4c3af31e64
Renato <ren.tito@yahoo.com.br>
2025-08-21 01:27:37 +0000

    project estrutura de dados

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+            .filter((pedidoParam) -> pedidoParam.getCategoria() == CategoriaProduto.LIVRO)
+            .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Optional<Produto> max = produtos.stream()
+            .max(Comparator.comparing(Produto::getPreco));
+        return max.get();
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        return produtos.stream()
+            .filter((pedidoParam) -> pedidoParam.getPreco() >= precoMinimo)
+            .toList();
+    }
+    
+}
---
+++ Streams_Collectors_Optional/preco_minimo/Produto.java
@@ -0,0 +1,53 @@
+import java.util.Locale;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+
+    @Override
+    public String toString() {
+        return String.format(new Locale("pt", "BR"), "[%d] %s %s R$ %.2f", 
+        codigo, nome, categoria, preco);
+    }
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
