02b288e754dea3c7fbd9e0fb507cdfadf4b2f16e
Renato <ren.tito@yahoo.com.br>
2025-09-26 23:53:43 +0000

    project estrutura de dados

---
+++ ddd/0/domain/Cliente.java
@@ -0,0 +1,18 @@
+package domain;
+
+
+public class Cliente {
+
+
+    private final String id;
+    private final String nome;
+
+
+    public Cliente(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+
+    // getters e setters
+}
---
+++ ddd/0/infrastructure/PedidoRepository.java
@@ -0,0 +1,25 @@
+package infrastructure;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import domain.Pedido;
+
+
+public class PedidoRepository {
+
+
+    private final Map<String, Pedido> pedidos = new HashMap<>();
+
+
+    public void salvar(Pedido pedido) {
+        // implementar
+    }
+
+
+    public Pedido buscarPorId(String id) {
+        // implementar
+    }
+}
